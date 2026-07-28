f5d1cc135a9dcd7b38263f2fe4d1f95da3b7e078
Wanderson Saraiva Torres <wanderson_st@hotmail.com.br>
2026-07-28 16:40:32 -0300

    melhora do css adicionado cards e animações

--- js/index.js
+++ js/index.js
@@ -12,3 +12,31 @@ botao.addEventListener('click', () => {
 
     }
  });
+
+//puxa todos os repositorios do github que estão visivel, inclusivel os fork.
+ //async function carregarProjetos() {
+   // try {
+       // const resposta = await fetch('https://api.github.com/users/wandersonstt/repos');
+
+       // const repositorios = await resposta.json();
+
+       // const container = document.getElementById('lista-projetos');
+
+       // repositorios.forEach(repo => {
+
+         //   const card = `
+              //  <article>
+                 //   <h3>${repo.name}</h3>
+                 //   <p>${repo.description || 'Projeto sem descrição no momento.'}</p>
+                 //   <a href="${repo.html_url}" target="_blank" style="color: var(--cor-texto); text-decoration: underline; font-weight: bold; margin-top: 10px; display: inline-block;">Ver código no GitHub</a>
+             //   </article>
+          //  `;
+
+          //      container.innerHTML += card;
+       // });
+ //   } catch (erro) {
+     //  console.error('Erro ao carregar os projetos:', erro);
+ //   }   
+// }
+
+// carregarProjetos();
